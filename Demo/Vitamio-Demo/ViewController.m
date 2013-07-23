e674eea4ae049a23a210aa46559395be5dc57344
nuoerlz <nuoerlz@gmail.com>
2013-07-23 13:37:12 +0800

    Update ViewController.m

--- Demo/Vitamio-Demo/ViewController.m
+++ Demo/Vitamio-Demo/ViewController.m
@@ -42,12 +42,12 @@
 	NSString *v = nil;
 
 //	v = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"video-demo.mkv"];
-//	v = @"http://meta.video.qiyi.com/242/de25dc2b5d385a8e27304d1e6dcd1a35.m3u8";
+	v = @"http://meta.video.qiyi.com/242/de25dc2b5d385a8e27304d1e6dcd1a35.m3u8";
 //	v = @"assets-library://asset/asset.MOV?id=112C86F5-1A5E-42EF-ADFA-0BE19C540665&ext=MOV";
 //	v = @"http://ipadlive.cntv.soooner.com/cctv_p2p_hdtaiqiu.m3u8";
 //	v = @"http://219.232.160.145:5080/livestream/esx7h676.ts"; // 打不开?
 //	v = @"http://119.97.131.69/hls/playlist.m3u8";
-	v = @"mms://cdnmms.cnr.cn/cnr001";
+//	v = @"mms://cdnmms.cnr.cn/cnr001";
 
 	playerCtrl.videoURL = [NSURL URLWithString:v];
 
